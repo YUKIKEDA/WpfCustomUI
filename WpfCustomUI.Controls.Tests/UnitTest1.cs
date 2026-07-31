b6fad5879582c09549a7bde5ab387a9443de942e
YUKIKEDA <yuuuu1008ki@gmail.com>
2026-08-01 00:11:15 +0900

    Add WpfCustomUI.Controls project and initial test setup
    - Introduced WpfCustomUI.Controls project with CustomControl1 and associated styles.
    - Added AssemblyInfo.cs for theme resource management.
    - Created Generic.xaml for control styling.
    - Established WpfCustomUI.Controls.Tests project with initial unit test structure.
    - Updated solution file to include new projects.

---
+++ WpfCustomUI.Controls.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace WpfCustomUI.Controls.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- WpfCustomUI.Controls/CustomControl1.cs
+++ WpfCustomUI.Controls/CustomControl1.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfCustomControls
+namespace WpfCustomUI.Controls
 {
     /// <summary>
     /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
@@ -18,14 +18,14 @@ namespace WpfCustomControls
     /// Add this XmlNamespace attribute to the root element of the markup file where it is 
     /// to be used:
     ///
-    ///     xmlns:MyNamespace="clr-namespace:WpfCustomControls"
+    ///     xmlns:MyNamespace="clr-namespace:WpfCustomUI.Controls"
     ///
     ///
     /// Step 1b) Using this custom control in a XAML file that exists in a different project.
     /// Add this XmlNamespace attribute to the root element of the markup file where it is 
     /// to be used:
     ///
-    ///     xmlns:MyNamespace="clr-namespace:WpfCustomControls;assembly=WpfCustomControls"
+    ///     xmlns:MyNamespace="clr-namespace:WpfCustomUI.Controls;assembly=WpfCustomUI.Controls"
     ///
     /// You will also need to add a project reference from the project where the XAML file lives
     /// to this project and Rebuild to avoid compilation errors:
